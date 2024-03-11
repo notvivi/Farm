259b1b6f2f9a1813fa387c99cc609137d4f81b54
tomanova <tomanova@spsejecna.cz>
2024-03-11 10:35:28 +0100

    changed variables

--- src/Horse.java
+++ src/Horse.java
@@ -1,2 +1,15 @@
-public class Horse {
+public class Horse extends Animal {
+    public Horse() {
+        super("n1ggorse","Josef",Size.BIG,"yes");
+    }
+
+    @Override
+    public String toString() {
+        return "Horse{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", specialCare='" + specialCare + '\'' +
+                '}';
+    }
 }
