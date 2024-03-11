fe5948e1b226f33e935b96630c48879aa57c3e9a
tomanova <tomanova@spsejecna.cz>
2024-03-11 10:21:00 +0100

    added constructor

--- src/SpiderLily.java
+++ src/SpiderLily.java
@@ -1,2 +1,15 @@
-public class SpiderLily {
+public class SpiderLily extends Flower{
+    public SpiderLily(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+
+    @Override
+    public String toString() {
+        return "SpiderLily{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", neededArea=" + neededArea +
+                ", chanceOfGrowth=" + chanceOfGrowth +
+                '}';
+    }
 }
