13c9a5e0830b5f63e6c6fe30b9373b57344d9be2
tomanova <tomanova@spsejecna.cz>
2024-03-11 10:36:14 +0100

    changed variables

--- src/Kangaroo.java
+++ src/Kangaroo.java
@@ -1,3 +1,16 @@
-public class Kangaroo {
+public class Kangaroo extends Animal {
 
+    public Kangaroo() {
+        super("boxer","juan",Size.SMALL,"ofc");
+    }
+
+    @Override
+    public String toString() {
+        return "Kangaroo{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", specialCare='" + specialCare + '\'' +
+                '}';
+    }
 }
