b605eada06c15b10687eb1693248573c1aac45e2
tomanova <tomanova@spsejecna.cz>
2024-03-11 10:41:20 +0100

    added method addAnimal

--- src/Farm.java
+++ src/Farm.java
@@ -2,9 +2,33 @@ import java.util.ArrayList;
 
 public class Farm {
     private int area;
-    private ArrayList <Flower> arrayList = new ArrayList<>();
+    private ArrayList <Flower> garden = new ArrayList<>();
+    private ArrayList <Animal> barn = new ArrayList<>();
 
-    public void addFlower(){
+    public void addFlower(Flower flower){
+       garden.add(flower);
+    }
+
+    public void addAnimal(Animal animal){
+        int counter = 0;
+        int counter2 = 0;
+        for(Animal a: barn){
+            if(a.getSize().equals(Size.BIG)){
+                counter++;
+                if(counter > 10){
+                    System.out.println("jsi opice, prodej se do otroctvi");
+                }else{
+                    barn.add(animal);
+                }
+            }else {
+               counter2++;
+               if(counter2 > 10){
+                   System.out.println("smoulo\uD83E\uDD76\uD83E\uDD76");
+               }else{
+                   barn.add(animal);
+               }
+            }
+        }
 
     }
 }
