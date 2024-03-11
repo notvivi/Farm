9532787e426d1b8a54d92ab18ecba7505354885e
tomanova <tomanova@spsejecna.cz>
2024-03-11 10:44:45 +0100

    added Getters and Setters

--- src/Animal.java
+++ src/Animal.java
@@ -12,6 +12,38 @@ public abstract class Animal {
         this.specialCare = specialCare;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public String getSpecialCare() {
+        return specialCare;
+    }
+
+    public void setSpecialCare(String specialCare) {
+        this.specialCare = specialCare;
+    }
+
     @Override
     public String toString() {
         return "Animal{" +
