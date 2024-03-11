2287dd1892d42eaa82e13bf081debc1aaa8926aa
tomanova <tomanova@spsejecna.cz>
2024-03-11 10:02:43 +0100

    added toString method nad constructor

---
+++ src/Rose.java
@@ -0,0 +1,15 @@
+public class Rose extends Flower{
+    public Rose(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+
+    @Override
+    public String toString() {
+        return "Rose{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", neededArea=" + neededArea +
+                ", chanceOfGrowth=" + chanceOfGrowth +
+                '}';
+    }
+}
